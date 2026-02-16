0fd82be85863276c293d956c88ad09278b458c59
Sreevani <sreevani.y@everest.engineering>
2026-02-16 16:12:52 +0530

    Implement configurations

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
-const taskRoute = require("./routes/taskRoute"); 
+
+const taskRoute = require("./routes/taskRoute");
+
 dotenv.config();
+
 const app: express.Application = express();
-const port=process.env.PORT;
+
+const port = Number(process.env.PORT) || 3000;
+
 app.use(express.json());
-app.use("/api", taskRoute); 
-app.get('/', (_req, _res) => {
-    _res.send("TypeScript With Express");
+app.use("/api", taskRoute);
+
+app.get('/', (_req, res) => {
+  res.send("TypeScript With Express");
 });
+
 app.listen(port, () => {
-    console.log(`http://localhost:${port}/`);
+  console.log(`http://localhost:${port}`);
 });
